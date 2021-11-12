c7fdd87b45555190251385dfea08fafdcc025171
Vishal062 <vs9425348@gmail.com>
2021-11-12 14:01:22 +0530

    Home page design ready

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { Homepage } from "./Components/Homeone";
+export default function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
